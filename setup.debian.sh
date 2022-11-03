7521e23ab45d406573a0dc0afa5c9e89cb3441a9
marcus <marcus.s.birgersson@gmail.com>
2022-11-03 20:51:08 +0100

    add install script for debian with install for vivaldi

---
+++ setup.debian.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pull dotfiles
+
+# Set keyboard layout
+
+# Install tmux
+
+# Install vivaldi
+sudo apt install wget -y
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
+
+sudo apt update && sudo apt install vivaldi-stable -y
+
+# Install alacritty (and cargo)
+
+
+sudo apt autoremove -y
